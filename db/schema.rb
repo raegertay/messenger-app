b765a0323635e0fe86269ae29ea074f0f9b72491
Raeger <raegertay89@gmail.com>
2018-04-17 22:06:44 +0800

    Add Subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417084430) do
+ActiveRecord::Schema.define(version: 20180417133737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20180417084430) do
   create_table "conversations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_subscriptions_on_conversation_id"
+    t.index ["user_id", "conversation_id"], name: "index_subscriptions_on_user_id_and_conversation_id", unique: true
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +49,6 @@ ActiveRecord::Schema.define(version: 20180417084430) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "subscriptions", "conversations"
+  add_foreign_key "subscriptions", "users"
 end
