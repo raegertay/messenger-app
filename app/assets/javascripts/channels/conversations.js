b7177175fd4092a36f2c98d2f720d7200463fcc3
Raeger <raegertay89@gmail.com>
2018-04-18 16:09:48 +0800

    Can only subscribe to one conversation channel at one time

--- app/assets/javascripts/channels/conversations.js
+++ app/assets/javascripts/channels/conversations.js
@@ -1,6 +1,11 @@
+if (conversationChannel !== null) {
+  conversationChannel.unsubscribe()
+  console.log('Unsubscribed')
+}
+
 if ($('#conversation_id').val()) {
   console.log('Subscribe to conversation_' + $('#conversation_id').val())
-  App.cable.subscriptions.create( { channel: 'ConversationChannel', conversation_id: $('#conversation_id').val() }, {
+  conversationChannel = App.cable.subscriptions.create( { channel: 'ConversationChannel', conversation_id: $('#conversation_id').val() }, {
     received: function(message) {
       console.log(message)
       $(".messages").append(`<p><b>${message.username}</b>: ${message.content}</p>`)
