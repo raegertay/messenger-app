54056fcce01741233c70ed57d5dddf42beeb530e
Raeger <raegertay89@gmail.com>
2018-04-22 11:55:33 +0800

    Style appearance and online indicator

--- app/assets/javascripts/channels/appearance.js
+++ app/assets/javascripts/channels/appearance.js
@@ -2,9 +2,11 @@ $(document).on('turbolinks:load', function() {
   App.cable.subscriptions.create( { channel: 'AppearanceChannel' }, {
     received: function(data) {
       if (data.type === 'appear') {
-        $(`#user_${data.user_id}`).addClass('text-success')
+        $(`#user_${data.user_id} svg`).css('visibility', 'visible')
+        $(`.conversation-user-${data.user_id} .online-indicator`).css('visibility', 'visible')
       } else {
-        $(`#user_${data.user_id}`).removeClass('text-success')
+        $(`#user_${data.user_id} svg`).css('visibility', 'hidden')
+        $(`.conversation-user-${data.user_id} .online-indicator`).css('visibility', 'hidden')
       }
     }
   })
