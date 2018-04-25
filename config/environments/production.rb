9c8c74523ecdedb4f8c22e0cd2b5aadf8d3d2004
Raeger <raegertay89@gmail.com>
2018-04-25 16:21:31 +0800

    Configure actioncable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://chitchat.raeger.com/cable" 
+  config.action_cable.url = "ws://chitchat.raeger.com/cable" 
   config.action_cable.allowed_request_origins = [ 'http://chitchat.raeger.com', /http:\/\/chitchat.raeger.*/, 'https://chitchat.raeger.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
